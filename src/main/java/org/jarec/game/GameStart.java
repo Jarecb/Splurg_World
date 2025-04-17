7543228ed8027643e38c82f01d44bb5e92823056
Jarecb <jarecb@gmail.com>
2025-04-17 15:14:19 +0300

    Added 3 hive option

--- src/main/java/org/jarec/game/GameStart.java
+++ src/main/java/org/jarec/game/GameStart.java
@@ -18,23 +18,45 @@ public class GameStart {
         var worldHeight = WorldFrame.getInstance().getWorldPanel().getWorldHeight();
         var hiveInset = Integer.parseInt(PropertyHandler.get("hive.default.position.inset", "150"));
 
-        Hive hiveOne = new Hive(new Location(hiveInset, hiveInset), new Color(255, 0, 0, 128), "Red Hive", false);
-        hiveOne.addEnergy(hiveEnergy);
-        Hives.getInstance().addHive(hiveOne);
+        var locationOne = new Location(0, 0);
+        var locationTwo = new Location(0, 0);
+        var locationThree = new Location(0, 0);
+        var locationFour = new Location(0, 0);
 
-        Hive hiveTwo = new Hive(new Location(worldWidth - hiveInset, worldHeight - hiveInset), new Color(0, 0, 255, 128),
+        if (hiveCount == 2) {
+            locationOne = new Location(hiveInset, worldHeight / 2);
+            locationTwo = new Location(worldWidth - hiveInset, worldHeight / 2);
+        } else if (hiveCount == 3) {
+            locationOne = new Location(hiveInset, worldHeight - hiveInset);
+            locationTwo = new Location(worldWidth - hiveInset, worldHeight - hiveInset);
+            locationThree = new Location(worldWidth / 2, hiveInset);
+        } else if (hiveCount == 4) {
+            locationOne = new Location(hiveInset, worldHeight - hiveInset);
+            locationTwo = new Location(worldWidth - hiveInset, worldHeight - hiveInset);
+            locationThree = new Location(hiveInset, hiveInset);
+            locationFour = new Location(worldWidth - hiveInset, hiveInset);
+        }
+
+        Hive hiveOne = new Hive(locationOne, new Color(255, 0, 0, 128), "Red Hive", false);
+        hiveOne.addEnergy(hiveEnergy);
+
+        Hive hiveTwo = new Hive(locationTwo, new Color(0, 0, 255, 128),
                 "Blue Hive", false);
         hiveTwo.addEnergy(hiveEnergy);
+
+        Hive hiveThree = new Hive(locationThree, new Color(255, 255, 0, 128), "Yellow Hive", false);
+        hiveThree.addEnergy(hiveEnergy);
+
+        Hive hiveFour = new Hive(locationFour, new Color(0, 255, 0, 128),
+                "Green Hive", false);
+        hiveFour.addEnergy(hiveEnergy);
+
+        Hives.getInstance().addHive(hiveOne);
         Hives.getInstance().addHive(hiveTwo);
-
-        if (hiveCount == 4) {
-            Hive hiveThree = new Hive(new Location(worldWidth - hiveInset, hiveInset), new Color(255, 255, 0, 128), "Yellow Hive", false);
-            hiveThree.addEnergy(hiveEnergy);
+        if (hiveCount >= 3) {
             Hives.getInstance().addHive(hiveThree);
-
-            Hive hiveFour = new Hive(new Location(hiveInset, worldHeight - hiveInset), new Color(0, 255, 0, 128),
-                    "Green Hive", false);
-            hiveFour.addEnergy(hiveEnergy);
+        }
+        if (hiveCount == 4) {
             Hives.getInstance().addHive(hiveFour);
         }
 
